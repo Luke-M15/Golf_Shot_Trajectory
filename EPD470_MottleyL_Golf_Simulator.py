60cecd5f2e9a3af9ac42f0b8692375b4054bb059
Luke-M15 <mottley15@gmail.com>
2018-04-30 22:27:03 -0500

    intial commit
    intial commit of the main scricpt for my golf shot simulator

---
+++ EPD470_MottleyL_Golf_Simulator.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Luke Mottley
+EPD 470 Final Project - Golf Simulator
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+if __name__ = "__main__":
+    
