e1c84b7df7556361bcccd3c3851f1787217b08da
Luke-M15 <mottley15@gmail.com>
2018-04-30 22:49:44 -0500

    correction
    corrected if main statement

--- EPD470_MottleyL_Golf_Simulator.py
+++ EPD470_MottleyL_Golf_Simulator.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
-if __name__ = "__main__":
+if __name__ == "__main__":
+    
+    df_clubs = pd.read_csv(r'C:\Users\motts\Documents\Grad School\BoxSync\EPD 470\Final Project\Golf_CLub_data.csv')
+    v_swing = 100
     
